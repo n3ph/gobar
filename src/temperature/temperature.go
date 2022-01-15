bd60f8e3cc4ea965c3848c8188b16163fda08749
Michael Gerlach <n3ph@cccfr.de>
2022-01-15 21:50:40 +0100

    Add infinite loop to `temperature.Update()`
    Signed-off-by: Michael Gerlach <n3ph@cccfr.de>

--- src/temperature/temperature.go
+++ src/temperature/temperature.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"time"
 
 	"github.com/shirou/gopsutil/host"
 )
@@ -13,21 +14,23 @@ type Temperature struct {
 }
 
 func (temperature *Temperature) Update(drift chan bool) {
-	temperature_new := &Temperature{}
-	sensors, err := host.SensorsTemperatures()
-	if err != nil {
-		panic(err)
-	}
-
-	for _, sensor := range sensors {
-		if sensor.SensorKey == "amdgpu_edge_input" {
-			temperature_new.value = sensor.Temperature
+	for range time.Tick(time.Second) {
+		temperature_new := &Temperature{}
+		sensors, err := host.SensorsTemperatures()
+		if err != nil {
+			panic(err)
 		}
-	}
 
-	if !reflect.DeepEqual(temperature, temperature_new) {
-		temperature.value = temperature_new.value
-		drift <- true
+		for _, sensor := range sensors {
+			if sensor.SensorKey == "amdgpu_edge_input" {
+				temperature_new.value = sensor.Temperature
+			}
+		}
+
+		if !reflect.DeepEqual(temperature, temperature_new) {
+			temperature.value = temperature_new.value
+			drift <- true
+		}
 	}
 }
 
