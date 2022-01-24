7fe72b53a4e0b1d8446eaf4e20bd83418f4ccdbd
Michael Gerlach <n3ph@cccfr.de>
2022-01-24 22:08:37 +0100

    refactor
    Signed-off-by: Michael Gerlach <n3ph@cccfr.de>

---
+++ pkg/temperature/temperature.go
@@ -0,0 +1,62 @@
+package temperature
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/shirou/gopsutil/host"
+)
+
+type Temperature struct {
+	sensor host.TemperatureStat
+	value  float64
+}
+
+func New(device string) (temperature Temperature, err error) {
+
+	if !(len(device) > 0) {
+		err = fmt.Errorf("BUG: device is empty string")
+		return
+	}
+
+	var sensors []host.TemperatureStat
+	sensors, err = host.SensorsTemperatures()
+	if err != nil {
+		return
+	}
+
+	for _, sensor := range sensors {
+		if sensor.SensorKey == device {
+			temperature.sensor = sensor
+			return
+		}
+	}
+
+	err = fmt.Errorf("get temperature stats: device not found: %s", device)
+	return
+}
+
+func (temperature *Temperature) Update(quit chan struct{}, duration time.Duration, value chan string, err chan error) {
+	for range time.Tick(duration) {
+		select {
+		case <-quit:
+			return
+		default:
+			temperature_new := Temperature{}
+			temperature_new.value = temperature.sensor.Temperature
+			if temperature.value != temperature_new.value {
+				temperature.value = temperature_new.value
+				value <- temperature.str()
+			}
+		}
+	}
+}
+
+func (temperature *Temperature) str() string {
+	temperatureStr := fmt.Sprintf("%.f", temperature.value) + "°C"
+	temperatureIcon := "🌡️"
+
+	temperatureList := []string{temperatureIcon, temperatureStr}
+	return strings.Join(temperatureList, " ")
+}
---
+++ pkg/temperature/temperature_test.go
@@ -0,0 +1,153 @@
+package temperature
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/shirou/gopsutil/host"
+)
+
+func TestNew(t *testing.T) {
+	type args struct {
+		device string
+	}
+
+	tests := []struct {
+		name            string
+		args            args
+		wantTemperature Temperature
+		wantErr         bool
+	}{
+		{"empty", args{device: ""}, Temperature{sensor: host.TemperatureStat{}, value: 0}, true},
+		{"unknown", args{device: "unknown"}, Temperature{sensor: host.TemperatureStat{}, value: 0}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotTemperature, err := New(tt.args.device)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotTemperature, tt.wantTemperature) {
+				t.Errorf("New() = %v, want %v", gotTemperature, tt.wantTemperature)
+			}
+		})
+	}
+}
+
+func BenchmarkNew(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		New("amdgpu_edge_input")
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	type args struct {
+		quit     chan struct{}
+		duration time.Duration
+		value    chan string
+		err      chan error
+	}
+
+	temperature, err := New("amdgpu_edge_input")
+	if err != nil {
+		t.Errorf("Unable to host temperature object: %s", err)
+	}
+
+	temperatureArgs := args{}
+	temperatureArgs.quit = make(chan struct{})
+	temperatureArgs.duration = time.Millisecond
+	temperatureArgs.value = make(chan string)
+	temperatureArgs.err = make(chan error)
+
+	tests := []struct {
+		name        string
+		temperature *Temperature
+		args        args
+	}{
+		{"goroutine", &temperature, temperatureArgs},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			go tt.temperature.Update(tt.args.quit, tt.args.duration, tt.args.value, tt.args.err)
+
+			loop := true
+			for loop {
+				select {
+				case err := <-tt.args.err:
+					t.Error(err)
+				case value := <-tt.args.value:
+					if !(len(value) > 0) {
+						t.Errorf("Unable to retrieve temperature string")
+					}
+					loop = false
+				}
+			}
+			close(tt.args.quit)
+			select {
+			case _, ok := (<-tt.args.value):
+				if ok {
+					t.Errorf("groutine not cleaned up properly")
+				}
+				break
+			default:
+			}
+		})
+	}
+}
+
+func BenchmarkUpdate(b *testing.B) {
+	type args struct {
+		quit     chan struct{}
+		duration time.Duration
+		value    chan string
+		err      chan error
+	}
+
+	temperature, err := New("amdgpu_edge_input")
+	if err != nil {
+		b.Errorf("Unable to host temperature object: %s", err)
+	}
+
+	temperatureArgs := args{}
+	temperatureArgs.quit = make(chan struct{})
+	temperatureArgs.duration = time.Millisecond
+	temperatureArgs.value = make(chan string)
+	temperatureArgs.err = make(chan error)
+
+	type quit struct{}
+	quitStruct := quit{}
+	for n := 0; n < b.N; n++ {
+		go temperature.Update(temperatureArgs.quit, temperatureArgs.duration, temperatureArgs.value, temperatureArgs.err)
+		temperatureArgs.quit <- quitStruct
+	}
+}
+
+func TestStr(t *testing.T) {
+	tests := []struct {
+		name        string
+		temperature *Temperature
+		want        string
+	}{
+		{"emptyStr", &Temperature{}, "🌡️ 0°C"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.temperature.str(); got != tt.want {
+				t.Errorf("Temperature.str() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkStr(b *testing.B) {
+	temperature, err := New("amdgpu_edge_input")
+	if err != nil {
+		b.Errorf("Unable to host temperature object: %s", err)
+	}
+
+	for n := 0; n < b.N; n++ {
+		_ = temperature.str()
+	}
+}
